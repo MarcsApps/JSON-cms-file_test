07c5dc573d306a9164920431145be318629c92b4
Marc Anders <marcsapps1@gmail.com>
2017-01-11 16:33:36 -0600

    Refactor addClick and loadJSON->Html into function reduced code repeats and made more reusable/modular. Removed console logs.

--- script.js
+++ script.js
@@ -2,68 +2,35 @@
 window.onload = function(){  // WRAP EVERYTHING.
 
 // global only to test - TODO
-// TODO
-    var jdata = "";
  
-    // load json without caching
-
-
-// button for saving to server
-var saveBut = document.getElementById('mySave')
-
-saveBut.addEventListener('click',function(event){
-
-});
-
-
-
-// save data
-function saveData(jsondata) {
-    $.post('js/postjson.php', {json : JSON.stringify(jsondata)},
-        function (data, textStatus, jqXHR){}
-    );
-}
-
-
-
-  //your code here
+ var jdata = "";
  
-var myEl = document.getElementById('myEl');
-myEl.addEventListener('click', function(event) {
-    // event.preventDefault();
-    console.log(`####  ${new Date().getTime()} 1 Calling Load F()`);
-    readData("p0",0);
-        console.log("2 read data");
-    
-}, false);
-        console.log("5 after event");
+ 
+myOnClicks("b1","p0",0);
+myOnClicks("b2","p1",0);
 
-
+function myOnClicks(eventID,elementTarget,indexHTML){
+    $('#'+ eventID ).on('click', function(){
+        readData(elementTarget,indexHTML);
+    })
+}; 
 
 function readData(elM,index) {
-    var nonCacheableURL = 'http://localhost:3000/pages?nocache=' + (new
-        Date()).getTime();
-        console.log("1 getJson");
+    var nonCacheableURL = 'http://localhost:3000/pages?nocache=' + (new 
+            Date()).getTime();   // load json without caching
         $.getJSON(nonCacheableURL, function(json) {
-        jdata = json[index].text;
-        console.log("3 json returned");
-        
+        jdata = json[index].text;      
         return jdata;
       })
 
         .done(function(){
-        console.log("4 done");
-            
-    console.log(`####  ${new Date().getTime()}  ${jdata}`);
-    var myP1 = document.getElementById(elM);
-    myP1.innerHTML=jdata;
-    $('#p0').innerHTML="" ;
-    
-    });
-    //return jdata; // dont need return as only returns empty (async)
+        var myP1 = document.getElementById(elM);
+        myP1.innerHTML=jdata;
+        $('#p0').innerHTML="" ;
+    }); 
 };
 
-} // onload - wrap whole script
+}; // onload - wrap whole script
 
 
 
